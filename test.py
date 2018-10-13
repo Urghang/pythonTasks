0cb686a3808c0ac534d458d0f1beb4d55d7c340a
Urghang <aleks_shura@mail.ru>
2018-10-13 15:33:54 +0200

    2

--- test.py
+++ test.py
@@ -1,36 +1,23 @@
-scores = []
-choice = None
+characters = {"Сила": 0, "Здоровье": 0, "Мудрость": 0, "Ловкость": 0}
+points = 30
 
-while choice != "0":
-    print(
-        """
-        
-        Рекорды 2.0
-        0 - Выйти
-        1 - Показать рекорды
-        2 - Добавить рекорд
-        """
-    )
+print("Вам доступно", points, "очков! \n \
+      Вы можете распределить их между основными характеристиками вашего персонажа\n \
+      ваши характеристики:")
 
-    choice = input("Ваш выбор: ")
-    print()
-    # выход
-    if choice == "0":
-        print("До свидания.")
-    # вывод рекордов
-    elif choice == "1":
-        print("Рекорды\n")
-        print("ИМЯ\tРЕЗУЛЬТАТ")
-        for entry in scores:
-            score, name = entry
-            print(name, "\t\t", score)
-    # ввод рекорда
-    elif choice == "2":
-        name = input("Введите имя игрока: ")
-        score = int(input("Введите его результат: "))
-        entry = (score, name)
-        scores.append(entry)
-        scores.sort(reverse=True)
-        scores = scores[:5]
-    else:
-        print("Извините, в меню нет пункта", choice)
+for key in characters:
+    print(key, ":", characters[key])
+
+print(
+    """
+    Выход 0
+    Распределить очки характеристик 1
+    Убрать очки характеристик 2
+    
+    """
+)
+
+variant = input("Ваш выбор; ")
+
+if variant == "1":
+    print("1")
